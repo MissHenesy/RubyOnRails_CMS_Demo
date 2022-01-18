59ae0b90c0420f9089fd62609780c29c8413f6da
Susan Henesy <shenesy@maidpro.com>
2022-01-18 10:52:56 -0500

    Moves startup js to custom/index file

--- app/javascript/custom/index.js
+++ app/javascript/custom/index.js
@@ -1,6 +1,9 @@
-$( document ).on('turbolinks:load', function() {
-  console.log('*** FIRED TURBOLINKS:LOAD ***');
-  // We can handle "document.ready" type actions here
-  //$('#carouselImageListItems').carousel();
-  //$('#carouselExampleIndicators').carousel();
-});
+// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
+document.addEventListener("turbolinks:load", () => {
+    // Display Bootstrap dropdown elements on hover
+    $('.navbar .dropdown').hover(function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+    }, function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+    });
+})
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,13 +33,3 @@ import "controllers"
 // in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
 global.$ = global.jQuery = jQuery;
 window.$ = window.jQuery = jQuery;
-
-// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
-document.addEventListener("turbolinks:load", () => {
-    // Display Bootstrap dropdown elements on hover
-    $('.navbar .dropdown').hover(function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-    }, function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
-    });
-})
